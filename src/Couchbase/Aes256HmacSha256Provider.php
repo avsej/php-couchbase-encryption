e22542e2059b8c8fc6513b07d944bb6d32abb46b
Sergey Avseyev <sergey.avseyev@gmail.com>
2018-04-13 00:11:44 +0300

    Implement AES-256-HMAC-SHA256 provider

---
+++ src/Couchbase/Aes256HmacSha256Provider.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Couchbase;
+
+/**
+ * Provides wrapper over openssl library for Couchbhase field encryption.
+ */
+class Aes256HmacSha256Provider extends CryptoProvider
+{
+    /**
+     * @var KeyProvider the key provider
+     */
+    private $keyProvider;
+
+    /**
+     * @var string the key identifier for HMAC signature
+     */
+    private $hmacKeyId;
+
+    /**
+     * Create instance of the OpenSSL crypto provider
+     *
+     * @param KeyProvider the key provider
+     */
+    public function __construct(KeyProvider $keyProvider, string $hmacKeyId)
+    {
+        $this->keyProvider = $keyProvider;
+        $this->hmacKeyId = $hmacKeyId;
+    }
+
+    /**
+     * Loads key from the key provider
+     */
+    public function loadKey(int $keyType, string $keyId)
+    {
+        return $this->keyProvider->getKey($keyType, $keyId);
+    }
+
+    public function generateIV()
+    {
+        return openssl_random_pseudo_bytes(16);
+    }
+
+    public function encrypt(string $bytes, string $key, string $iv = null)
+    {
+        return openssl_encrypt($bytes, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+    }
+
+    public function decrypt(string $bytes, string $key, string $iv = null)
+    {
+        return openssl_decrypt($bytes, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+    }
+
+    public function sign(array $bytes)
+    {
+        $ctx = hash_init('sha256', HASH_HMAC, $this->keyProvider->getKey(0, $this->hmacKeyId));
+        foreach ($bytes as $chunk) {
+            hash_update($ctx, $chunk);
+        }
+        return hash_final($ctx, true);
+    }
+
+    public function verifySignature(array $bytes, string $signature)
+    {
+        $ctx = hash_init('sha256', HASH_HMAC, $this->keyProvider->getKey(0, $this->hmacKeyId));
+        foreach ($bytes as $chunk) {
+            hash_update($ctx, $chunk);
+        }
+        $actual = hash_final($ctx, true);
+        return $actual == $signature;
+    }
+};
---
+++ tests/Couchbase/Aes256HmacSha256ProviderTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Couchbase\Tests;
+
+use Couchbase\Aes256HmacSha256Provider;
+use Couchbase\KeyProvider;
+use Couchbase\Cluster;
+use Couchbase\Bucket;
+
+use PHPUnit_Framework_TestCase;
+
+// phpcs:disable PEAR.Commenting
+
+final class InsecureKeyProvider implements KeyProvider
+{
+    public function getKey(int $type, string $id)
+    {
+        switch ($id) {
+            case 'mypublickey':
+                return "!mysecretkey#9^5usdk39d&dlf)03sL";
+            case 'HMAC_KEY_ID':
+                return 'myauthpassword';
+            default:
+                throw new InvalidArgumentException("Unknown key '$id");
+        }
+    }
+}
+
+final class Aes256HmacSha256ProviderTest extends PHPUnit_Framework_TestCase
+{
+    private $cluster = null;
+    private $bucket = null;
+
+    protected function setUp()
+    {
+        $this->cluster = new Cluster('couchbase://localhost');
+        $this->cluster->authenticateAs('Administrator', 'password');
+        $this->bucket = $this->cluster->openBucket('default');
+        $this->bucket->registerCryptoProvider(
+            'AES-256-HMAC-SHA256',
+            new Aes256HmacSha256Provider(new InsecureKeyProvider(), "HMAC_KEY_ID")
+        );
+    }
+
+    protected function tearDown()
+    {
+        // $this->bucket->unregisterCryptoProvider('AES-256-HMAC-SHA256');
+    }
+
+    protected function storeEncrypted(string $id, $payload, array $options)
+    {
+        $document = ['message' => $payload];
+        $encrypted = $this->bucket->encryptDocument($document, $options);
+        $this->bucket->upsert($id, $encrypted);
+    }
+
+    protected function getEncrypted(string $id)
+    {
+        $encrypted = $this->bucket->get($id);
+        $this->assertNotNull($encrypted->value, 'document should not be null');
+        $document = $this->bucket->decryptDocument($encrypted->value);
+        $this->assertArrayHasKey('message', $document);
+        return $document['message'];
+    }
+
+    public function testEncryptDecrypt()
+    {
+        $fieldOptions = [
+            [
+                'name' => 'message',
+                'kid' => 'mypublickey',
+                'alg' => 'AES-256-HMAC-SHA256'
+            ]
+        ];
+
+        $this->storeEncrypted(
+            'secret-1',
+            'The old grey goose jumped over the wrickety gate.',
+            $fieldOptions
+        );
+        $this->assertEquals(
+            'The old grey goose jumped over the wrickety gate.',
+            $this->getEncrypted('secret-1')
+        );
+
+        $this->storeEncrypted('secret-2', 10, $fieldOptions);
+        $this->assertEquals(10, $this->getEncrypted('secret-2'));
+
+        $this->storeEncrypted('secret-3', '10', $fieldOptions);
+        $this->assertEquals('10', $this->getEncrypted('secret-3'));
+
+        $this->storeEncrypted(
+            'secret-4',
+            ["The", "Old", "Grey","Goose", "Jumped", "over", "the", "wrickety", "gate"],
+            $fieldOptions
+        );
+        $this->assertEquals(
+            ["The", "Old", "Grey","Goose", "Jumped", "over", "the", "wrickety", "gate"],
+            $this->getEncrypted('secret-4')
+        );
+
+        $this->storeEncrypted(
+            'secret-5',
+            [
+               'myValue' => 'The old grey goose jumped over the wrickety gate.',
+               'myInt' => 10
+            ],
+            $fieldOptions
+        );
+        $this->assertEquals(
+            [
+               'myValue' => 'The old grey goose jumped over the wrickety gate.',
+               'myInt' => 10
+            ],
+            $this->getEncrypted('secret-5')
+        );
+    }
+}
